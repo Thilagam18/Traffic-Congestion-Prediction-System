48ce8217a1fd759789581c90bd41064753e4b022
Thilagam18 <thilagam18sp@gmail.com>
2026-06-15 12:42:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import RouteOptimization from "./pages/RouteOptimization";
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/route"
+          element={<RouteOptimization />}
+        />
       </Routes>
     </BrowserRouter>
   );
