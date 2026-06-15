b02bcba80bcc3e1c3e7bf66d0bbdd895000452a7
Thilagam18 <thilagam18sp@gmail.com>
2026-06-15 13:05:00 +0530

    Add Register route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import RouteOptimization from "./pages/RouteOptimization";
 
@@ -9,7 +10,17 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
         <Route
           path="/route"
           element={<RouteOptimization />}
